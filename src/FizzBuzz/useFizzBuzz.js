f0def19c8d89185ae6f629e63b2e76c5f9f887a7
Will Cooley <william.cooley@gmail.com>
2025-03-17 12:35:30 -0400

    fail: the fizzbuzz hook > when playing > updates the input

--- src/FizzBuzz/useFizzBuzz.js
+++ src/FizzBuzz/useFizzBuzz.js
@@ -1,8 +1,17 @@
+import {useState} from 'react'
+
 export function useFizzBuzz() {
+    
+    const [result, setResult] = useState(null)
+
     return {
 
         input: null,
-        result: null
+        result: result,
+
+        play: ()=>{
+            setResult('yes')
+        }
 
     };
 }
--- src/FizzBuzz/useFizzBuzz.test.js
+++ src/FizzBuzz/useFizzBuzz.test.js
@@ -1,5 +1,6 @@
+import { describe } from "vitest";
 import {useFizzBuzz} from "./useFizzBuzz.js";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 
 
 describe('the fizzbuzz hook',()=>{
@@ -10,11 +11,29 @@ describe('the fizzbuzz hook',()=>{
     })
     
     it('has an input',()=>{
-        expect(subject.result.current.input).toBeDefined()
+        expect(subject.result.current.input).toBeNull()
     })
 
     it('has a result',()=>{
-        expect(subject.result.current.result).toBeDefined()
+        expect(subject.result.current.result).toBeNull()
     })
+
+    describe('when playing',()=>{
+
+        it('updates the input', ()=>{
+            act(()=>{
+                subject.result.current.setInput('4')
+            })
+            expect(subject.result.current.input).toEqual('4')
+        })
+
+        it('displays the result', ()=>{
+            act(()=>{
+                subject.result.current.play()
+            })
+            expect(subject.result.current.result).toEqual('yes')
+        })
+    })
+
 })
 
