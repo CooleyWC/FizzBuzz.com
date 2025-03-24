5c6c8548a1ae74fa95a7821dcb5d885f5824af89
Will Cooley <william.cooley@gmail.com>
2025-03-24 12:23:39 -0400

    clean: move label style to stylesheet

--- src/FizzBuzz/FizzBuzz.jsx
+++ src/FizzBuzz/FizzBuzz.jsx
@@ -1,4 +1,6 @@
 import {useFizzBuzz} from "./useFizzBuzz.js";
+import './FizzBuzz.css';
+
 
 export function FizzBuzz() {
     const {input, setInput, result, play} = useFizzBuzz();
@@ -6,8 +8,9 @@ export function FizzBuzz() {
         <form onSubmit={submit}>
             <input
                 value={input || ""}
-                onChange={e => setInput(e.target.value)} />
-            <label style={{display: 'none'}}>Play</label>
+                onChange={e => setInput(e.target.value)} 
+                />
+            <label className='play-label'>Play</label>
                 <input style={{margin: '0 0.5em'}} type='submit' value='Play'/>
             <div style={{margin: '2em auto'}}>{result}</div>
         </form>
